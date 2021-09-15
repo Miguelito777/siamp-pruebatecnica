960aed9d26e85292622d359657cf4f9dbf077ca3
Tu Nombre <you@example.com>
2021-09-15 08:28:52 -0600

    add cros

--- server.js
+++ server.js
@@ -80,15 +80,15 @@ app.listen(PORT,function(){
 });*/
 
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
-//var corsOptions = {
-//  origin: "http://localhost:8081"
-//};
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
 
-//app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
